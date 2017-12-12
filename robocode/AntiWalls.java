028e34215971a6fb6d3b8aedbe7b697a52df5286
Till Zoppke <till.zoppke@fu-berlin.de>
2017-12-13 00:45:17 +0100

    adding robocode sample bot

---
+++ robocode/AntiWalls.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2001-2017 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
+ */
+package myrobot;
+
+import robocode.*;
+import java.lang.Math;
+
+/**
+ * AntiWalls - a sample robot by kinnla, 
+ * winning approx. 74% of the score vs. Wall
+ */
+public class AntiWalls extends RateControlRobot {
+
+	boolean peek;
+
+	public void run() {
+		setBodyColor(Color.orange);
+		setGunColor(Color.orange);
+		setRadarColor(Color.orange);
+		setBulletColor(Color.orange);
+		setScanColor(Color.orange);
+
+		peek = false;
+
+		turnLeft(getHeading() % 90);
+		move();
+		turnGunLeft(135);
+		turnRight(90);
+
+		while (true) {
+			scan();
+			if (peek) {
+				move();
+				turnRight(90);
+				ahead(50);
+				peek = false;
+			}
+		}
+	}
+
+	public void onScannedRobot(ScannedRobotEvent e) {
+		if (e.getDistance() < 5 * getWidth()) {
+			peek = true;
+			fire(3);
+		}
+	}
+	
+	private void move() {
+		if (Math.abs(getHeading()) < 10) {
+			ahead(getBattleFieldHeight() - getY() - 2 * getWidth());
+		} else if (Math.abs(180 - getHeading()) < 10) {
+			ahead(getY() - 2 * getWidth());
+		} else if (Math.abs(90 - getHeading()) < 10) {
+			ahead(getBattleFieldWidth() - getX() - 2 * getWidth());
+		} else {
+			ahead(getX() - 2 * getWidth());
+		}
+	}
+}
